f4fd1128a5564112e7140c827cb80e1bb7aeee1a
cooddoocj <41693349+cooddoocj@users.noreply.github.com>
2019-06-23 17:16:51 +0700

    Update truyenCV_downloader.user.js

--- truyenCV_downloader.user.js
+++ truyenCV_downloader.user.js
@@ -43,6 +43,7 @@
     function cleanHtml(str) {
         str = str.replace(/\s*Chương\s*\d+\s?:[^<\n]/, '');
         str = str.replace(/\"\. \. \.\"/g, '"Lặng!"');
+        str = str.replace(/đồ.{1,3}vật/g, 'đồ vật');
         str = str.replace(/[^\x09\x0A\x0D\x20-\uD7FF\uE000-\uFFFD\u10000-\u10FFFF]+/gm, ''); // eslint-disable-line
         str = str.replace(/\s[a-zA-Z0-9]{6,8}(="")?\s/gm, function (key, attr) {
             if (attr) return ' ';
