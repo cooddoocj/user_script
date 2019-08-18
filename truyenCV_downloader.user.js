f79dde174d9c7817a77b3e30b96f357917c1cc12
cooddoocj <41693349+cooddoocj@users.noreply.github.com>
2019-08-18 10:07:23 +0700

    Update truyenCV_downloader.user.js

--- truyenCV_downloader.user.js
+++ truyenCV_downloader.user.js
@@ -43,8 +43,8 @@
     converter = new RegExp('(' + converter + ')', 'i');
 
     function cleanHtml(str) {
-    	str = str.replace(/\.(?:\s*\.)+/, '...');
-    	str = str.replace(/"\.\.\."/, '"Lặng!"');
+    	str = str.replace(/\.(?:\s*\.)+/gm, '...');
+    	str = str.replace(/"\.\.\."/gm, '"Lặng!"');
         str = str.replace(/\s*Chương\s*\d+\s?:[^<\n]/, '');
         str = str.replace(/[^\x09\x0A\x0D\x20-\uD7FF\uE000-\uFFFD\u10000-\u10FFFF]+/gm, ''); // eslint-disable-line
         str = str.replace(/\s[a-zA-Z0-9]{6,8}(="")?\s/gm, function (key, attr) {
